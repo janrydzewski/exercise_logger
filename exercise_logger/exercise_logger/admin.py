5bf608bf283040e00082edc47f1768f48577c12a
j_rydzewski <52713164+janrydzewski@users.noreply.github.com>
2024-04-14 13:11:40 +0200

    add models

--- exercise_logger/exercise_logger/admin.py
+++ exercise_logger/exercise_logger/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Exercise, MuscleGroup, Workout, WorkoutExercise, ExerciseMuscleGroups, WorkoutTemplate, WorkoutTemplateExercises
 
-# Register your models here.
+admin.site.register(Exercise)
+admin.site.register(MuscleGroup)
+admin.site.register(Workout)
+admin.site.register(WorkoutExercise)
+admin.site.register(ExerciseMuscleGroups)
+admin.site.register(WorkoutTemplate)
+admin.site.register(WorkoutTemplateExercises)
--- exercise_logger/exercise_logger/models.py
+++ exercise_logger/exercise_logger/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length = 30)
+    
+class Exercise(models.Model):
+    name = models.CharField(max_length = 50)
+    
+class MuscleGroup(models.Model):
+    name = models.CharField(max_length = 50)
+    size = models.IntegerField()
+
+class Workout(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    
+class WorkoutExercise(models.Model):
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    reps = models.IntegerField()
+    weight = models.IntegerField()
+    
+class ExerciseMuscleGroups(models.Model):
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    muscle_group = models.ForeignKey(MuscleGroup, on_delete=models.CASCADE)
+    
+class WorkoutTemplate(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+class WorkoutTemplateExercises(models.Model):
+    workout_template = models.ForeignKey(WorkoutTemplate, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+
+
+    
