d409b6148edbe6a6dcf1bf6d7695f860fc369953
Radosław Sienkiewicz <rsienkiewicz88@gmail.com>
2024-04-17 06:46:45 +0200

    database update
    added database for tracking, migrated models to db, added User model to the admin panel

--- exercise_logger/exercise_logger/admin.py
+++ exercise_logger/exercise_logger/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Exercise, MuscleGroup, Workout, WorkoutExercise, ExerciseMuscleGroups, WorkoutTemplate, WorkoutTemplateExercises
+from .models import User, Exercise, MuscleGroup, Workout, WorkoutExercise, ExerciseMuscleGroups, WorkoutTemplate, WorkoutTemplateExercises
 
+admin.site.register(User)
 admin.site.register(Exercise)
 admin.site.register(MuscleGroup)
 admin.site.register(Workout)
